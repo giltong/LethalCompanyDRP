2a4b3c5aecd12987be9192527ca794ea0f842ab3
Gil <gilpowers2006@gmail.com>
2023-11-22 18:29:53 -0600

    Removed threading, added all mechanics into a monobehaviour

---
+++ LethalCompanyDRP/DiscordRP.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Discord;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LethalCompanyDRP;
+
+public class DiscordRP : MonoBehaviour
+{
+    private Discord.Discord _discord;
+    private ActivityManager _activityManager;
+    private readonly long _clientId = 1176318786194911254;
+    private Activity _activity = new() { Instance = true };
+    private bool _inGame;
+    private void Start()
+    {
+        _discord = new Discord.Discord(_clientId, (UInt64)CreateFlags.NoRequireDiscord);
+        _activityManager = _discord.GetActivityManager();
+        if (_activityManager == null) return;
+        _activityManager.RegisterSteam(1966720);
+        _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+        Plugin.log.LogInfo("Discord RPC Started");
+    }
+
+    private void Update()
+    {
+        try
+        {
+            _discord.RunCallbacks();
+            UpdateActivity();
+        }
+        catch(ResultException e)
+        {
+            Plugin.log.LogError("Discord throws a ResultException: " + e.Message);
+        }
+    }
+    
+    private void UpdateActivity()
+    {
+        if (_activityManager == null) return;
+    
+        CheckCurrentScene();
+        
+        _activity.Assets.LargeImage = "game_icon";
+        _activity.Assets.LargeText = "Lethal Company";
+        
+        if (_inGame)
+        {
+            _activity.Details = "Profit Quota: " + TimeOfDay.Instance.quotaFulfilled + " / " + TimeOfDay.Instance.profitQuota;
+            _activity.State = TimeOfDay.Instance.daysUntilDeadline + " days remaining";
+        }
+        else
+        {
+            _activity.State = "in Main Menu";
+            _activity.Secrets.Join = null;
+        }
+        
+        try
+        {
+            _activityManager.UpdateActivity(_activity, result => {});
+        }
+        catch (Exception e)
+        {
+            Plugin.log.LogError("Discord::UpdateActivity throws a " + e.GetType() + ":\n" + e.Message);
+            throw;
+        }
+    }
+    
+    private void CheckCurrentScene()
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name is "MainMenu")
+            {
+                if (_inGame)
+                {
+                    _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+                }
+                _inGame = false;
+            }
+            else if (SceneManager.GetSceneAt(i).name == "SampleSceneRelay" && SceneManager.GetSceneAt(i).isLoaded)
+            {
+                if (!_inGame)
+                {
+                    _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
+                }
+                _inGame = true;
+            }
+        } 
+    }
+}
--- LethalCompanyDRP/Plugin.cs
+++ LethalCompanyDRP/Plugin.cs
@@ -12,115 +12,19 @@ namespace LethalCompanyDRP
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-
         internal static ManualLogSource log;
         
-        private static Discord.Discord _discord;
-        private static ActivityManager _activityManager;
-        private static long _clientId = 1176318786194911254;
-        private static Activity _activity = new Activity { Instance = true };
-        private static bool InGame = false;
-        
-        
-        public Activity defaultActivity = new Activity()
-        {
-            State = "Test State",
-            Details = "Test Details",
-        };
-        private static void StartDiscordRPC()
-        {
-            /* Give some time for game to initialize */
-            Thread.Sleep(5000);
-            Plugin.log.LogInfo("Discord RPC Started");
-            _discord = new Discord.Discord(_clientId, (UInt64)CreateFlags.NoRequireDiscord);
-            _activityManager = _discord.GetActivityManager();
-            if (_activityManager == null) return;
-            _activityManager.RegisterSteam(1966720);
-            _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
-            UpdateActivity();
-            try
-            {
-                while (true)
-                {
-                    try
-                    {
-                        Thread.Sleep(500);
-                        _discord.RunCallbacks();
-                        UpdateActivity();
-                    }
-                    catch(ResultException e)
-                    {
-                        log.LogError("Discord throws a ResultException: " + e.Message);
-                    }
-                }
-            }
-            finally
-            {
-                _discord.Dispose();
-            }
-        }
-        
-        private static void UpdateActivity()
-        {
-            if (_activityManager == null) return;
-
-            CheckCurrentScene();
-            
-            _activity.Assets.LargeImage = "game_icon";
-            _activity.Assets.LargeText = "Lethal Company";
-            
-            if (InGame)
-            {
-                _activity.Details = "Profit Quota: " + TimeOfDay.Instance.quotaFulfilled + " / " + TimeOfDay.Instance.profitQuota;
-                _activity.State = TimeOfDay.Instance.daysUntilDeadline + " days remaining";
-            }
-            else
-            {
-                _activity.State = "in Main Menu";
-                _activity.Secrets.Join = null;
-            }
-            
-            try
-            {
-                _activityManager.UpdateActivity(_activity, result => {});
-            }
-            catch (Exception e)
-            {
-                log.LogError("Discord::UpdateActivity throws a " + e.GetType() + ":\n" + e.Message);
-                throw;
-            }
-        }
-
-        private static void CheckCurrentScene()
-        {
-            for (int i = 0; i < SceneManager.sceneCount; i++)
-            {
-                if (SceneManager.GetSceneAt(i).name is "MainMenu")
-                {
-                    if (InGame)
-                    {
-                        _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
-                    }
-                    InGame = false;
-                }
-                else if (SceneManager.GetSceneAt(i).name == "SampleSceneRelay" && SceneManager.GetSceneAt(i).isLoaded)
-                {
-                    if (!InGame)
-                    {
-                        _activity.Timestamps.Start = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds();
-                    }
-                    InGame = true;
-                }
-            } 
-        }
-
         private void Awake()
         {
-            Plugin.log = base.Logger;
-            Logger.LogInfo($"Started {PluginInfo.PLUGIN_GUID}");
-            Thread discordThread = new Thread(StartDiscordRPC);
-            discordThread.Start();
+            log = Logger;
+            log.LogInfo($"Started {PluginInfo.PLUGIN_GUID}");
         }
-        
+
+        private void OnDestroy()
+        {
+            GameObject discordGameObject = new GameObject();
+            DontDestroyOnLoad(discordGameObject);
+            discordGameObject.AddComponent<DiscordRP>();
+        }
     }
 }
